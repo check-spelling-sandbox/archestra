ed5230d70ff938dc653d830d055522b62eaf2894
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-21 06:59:12 -0400

    spelling: proxying
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- platform/backend/src/types/llm-providers/zhipuai/index.ts
+++ platform/backend/src/types/llm-providers/zhipuai/index.ts
@@ -1,6 +1,6 @@
 /**
  * NOTE: this is a bit of a PITA/verbose but in order to properly type everything that we are
- * proxing.. this is kinda necessary.
+ * proxying.. this is kinda necessary.
  *
  * the zhipu ai api doesn't expose a typescript sdk, so we define our own zod schemas..
  */
